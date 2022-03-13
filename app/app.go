5313178523bf7bffcfec647e110e84300babd916
Denys Sych <denis.feelin@gmail.com>
2022-03-13 16:08:25 +0200

    added gorilla/mux and several test routes

--- app/app.go
+++ app/app.go
@@ -1,16 +1,21 @@
 package app
 
 import (
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
 
 func Start() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/greet", greet)
-	mux.HandleFunc("/customers", getAllCustomers)
+	router := mux.NewRouter()
+	router.HandleFunc("/greet", greet).Methods(http.MethodGet)
+	router.HandleFunc("/customers", getAllCustomers).Methods(http.MethodGet)
+	router.HandleFunc("/api/time", getTime).Queries("tz", "{time_zone}").Methods(http.MethodGet)
+	router.HandleFunc("/customers", createCustomer).Methods(http.MethodPost)
+	//router.HandleFunc("/customers/{customer_id}", getCustomer).Methods(http.MethodGet) // any match
+	router.HandleFunc("/customers/{customer_id:[0-9]+}", getCustomer).Methods(http.MethodGet) // numeric only [pattern for route]
 
-	err := http.ListenAndServe("localhost:8000", mux)
+	err := http.ListenAndServe("localhost:8000", router)
 	if err != nil {
 		log.Fatal(err)
 	}
--- app/handlers.go
+++ app/handlers.go
@@ -3,7 +3,9 @@ package app
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
+	"time"
 )
 
 type Customer struct {
@@ -11,17 +13,42 @@ type Customer struct {
 	City string `json:"city"`
 }
 
+type TimeResponse struct {
+	CurrentTime string `json:"current_time"`
+}
 
 func greet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello world")
 }
 
 func getAllCustomers(w http.ResponseWriter, r *http.Request) {
-	customers := []Customer {
-		{Name: "Marko", City: "Dodo" },
-		{Name: "Donald", City: "Dodo" },
+	customers := []Customer{
+		{Name: "Marko", City: "Dodo"},
+		{Name: "Donald", City: "Dodo"},
 	}
 
 	w.Header().Add("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(customers)
+}
+
+func getTime(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	loc, err := time.LoadLocation(vars["time_zone"])
+	dt := time.Now()
+
+	if err != nil {
+		fmt.Fprint(w, dt.Format(time.RFC1123))
+		return
+	}
+
+	fmt.Fprint(w, dt.In(loc).Format(time.RFC1123))
+}
+
+func createCustomer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Post request received...")
+}
+
+func getCustomer(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fmt.Fprint(w, vars["customer_id"])
 }
